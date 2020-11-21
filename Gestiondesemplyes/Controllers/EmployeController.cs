15c23092e586527650312a33f521ae5c0c2cbeed
refka touati <touatirefkaa@gmail.com>
2020-11-21 11:20:22 +0100

    first commit

---
+++ Gestiondesemplyes/Controllers/EmployeController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gestiondesemplyes.Models;
+using Gestiondesemplyes.Models.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Gestiondesemplyes.Controllers
+{
+    public class EmployeController : Controller
+    {
+        // injectiion des dependances
+        readonly IRepository<Employee> employeeRepository;
+        public EmployeController(IRepository<Employee> empRepository)
+        {
+            employeeRepository= empRepository;
+        }
+        public ActionResult Index()
+        {
+            var employees = employeeRepository.GetAll();
+            ViewData["EmployeesCount"] = employees.Count();
+            ViewData["SalaryAverage"] = employeeRepository.SalaryAverage();
+            ViewData["MaxSalary"] = employeeRepository.MaxSalary();
+            ViewData["HREmployeesCount"] = employeeRepository.HrEmployeesCount();
+            return View( employees);
+        }
+
+        // GET: EmployeController/Details/5
+        public ActionResult Details(int id)
+        {
+            var employee = employeeRepository.FindByID(id);
+            return View(employee);
+        }
+
+        // GET: EmployeController/Create
+        public ActionResult Create()
+        {   
+            return View();
+        }
+
+        // POST: EmployeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee e)
+        {
+            try
+            {
+                employeeRepository.Add(e);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var employee = employeeRepository.FindByID(id);
+            return View(employee);
+        }
+
+        // POST: EmployeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Employee newemployee)
+        {
+            try
+            {
+                employeeRepository.Update(id, newemployee);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult Search(String term)
+        {
+            var result = employeeRepository.Search(term);
+            return View("Index", result);
+        }
+
+        // GET: EmployeController/Delete/5
+        
+        public ActionResult Delete(int id)
+        {
+            var employee = employeeRepository.FindByID(id);
+            return View(employee);
+        }
+
+        // POST: EmployeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Employee e)
+        {
+            try
+            {
+                employeeRepository.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+
+
+            
+        }
+        
+    }
+}
---
+++ Gestiondesemplyes/Models/Repositories/EmployeeRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestiondesemplyes.Models.Repositories
+{
+    public class EmployeeRepository : IRepository<Employee>
+    {
+        List<Employee> lemployees;
+        public EmployeeRepository()
+        {
+            lemployees = new List<Employee>(){
+                new Employee { Id = 1,Name = "Sofien ben ali", Departement = "comptabilité",Salary = 1000},
+                new Employee { Id = 2,Name = "Mourad triki", Departement = "HR",Salary = 1500},
+                new Employee { Id = 3,Name = "ali ben mohamed", Departement = "informatique",Salary = 1700},
+                new Employee { Id = 4,Name = "tarak aribi", Departement = "informatique",Salary = 1100} };
+        }
+        public void Add(Employee e)
+        {
+           lemployees.Add(e);
+        }
+
+        public void Delete(int id)
+        {
+            var emp = FindByID(id);
+            lemployees.Remove(emp);
+        }
+
+        public Employee FindByID(int id)
+        {
+            var emp = lemployees.FirstOrDefault(a => a.Id == id);
+            return emp;
+        }
+
+        public IList<Employee> GetAll()
+        {
+            return lemployees;
+        }
+
+        public int HrEmployeesCount()
+        {
+            return lemployees.Where(x => x.Departement == "HR").Count();
+        }
+
+        public double MaxSalary()
+        {
+            return lemployees.Max(x => x.Salary);
+        }
+
+        public double SalaryAverage()
+        {
+            return lemployees.Average(x => x.Salary);
+       }
+
+        public List<Employee> Search(string term)
+        {
+            if (!string.IsNullOrEmpty(term))
+                return lemployees.Where(a => a.Name.Contains(term)).ToList();
+            else
+                return lemployees;
+        }
+
+        public void Update(int id, Employee newemployee)
+        {
+            var emp = FindByID(id);
+
+            emp.Name = newemployee.Name;
+            emp.Departement = newemployee.Departement;
+            emp.Salary = newemployee.Salary;
+        }
+    }
+}
